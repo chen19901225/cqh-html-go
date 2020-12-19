df4f1579b4541073cba5d52554dfa3bbcf4a83a5
test <test@qq.com>
2020-12-20 00:08:48 +0800

    replace start_text with regex

--- src/config.ts
+++ src/config.ts
@@ -14,24 +14,15 @@ export interface IConfig extends vscode.WorkspaceConfiguration {
 export function getMatchText(cmd: ICommand, text: string): [boolean, string] {
     let line_text = text.trim();
     for (let start_text of cmd.start_text_list) {
-        if (!start_text.endsWith("(")) {
-            start_text = start_text + "("
+        // if (!start_text.endsWith("(")) {
+        //     start_text = start_text + "("
+        // }
+        var re = new RegExp(start_text);
+        var match = re.exec(line_text)
+        if (match) {
+            return [true, match[1]]
         }
-        if (line_text.startsWith(start_text)) {
-            let start_text_length = start_text.length;
-            let next_ch = line_text[start_text_length]; // 判断下一个字符是不是引号
-            let match_quote = ["'", '"']
-            for (let quote of match_quote) {
-                if (quote == next_ch) {
-                    let end_index = line_text.lastIndexOf(quote)
-                    if (end_index > -1 && end_index > start_text_length) { // 获取结束的引号的位置
-                        
-                        let match_text = line_text.slice(start_text_length + 1, end_index + 1)
-                        return [true, match_text]
-                    }
-                }
-            }
-        }
+
     }
 
     return [false, ""]
@@ -40,7 +31,7 @@ export function getSnippetForExt(ext: string): ICommand | null {
     if (!ext.startsWith(".")) { // 不是ext二十路径
         ext = extname(ext);
     }
-    let config = vscode.workspace.getConfiguration("cqh-goto") as IConfig;
+    let config = vscode.workspace.getConfiguration("cqh-html-go") as IConfig;
     let snippet = null;
     for (let i = 0; i < config.byExt.length; i++) {
         let current_snippet = config.byExt[i];
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as static_var from "./static_var"
+import { CqhCodeLenProvider } from './providers/cqh_code_len_provider';
+import { CqhRunner } from './runner/cqh_runner';
+import { cqh_goto_list } from './handler/list';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,20 +12,48 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cqh-html-go" is now active!');
+	// console.log('Congratulations, your extension "cqh-html-go" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cqh-html-go.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// // The command has been defined in the package.json file
+	// // Now provide the implementation of the command with registerCommand
+	// // The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('cqh-html-go.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cqh_html_go!');
-	});
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from cqh_html_go!');
+	// });
+
+	// context.subscriptions.push(disposable);
+
+
+	let docSelector = {
+		language: '*',
+		scheme: 'file',
+	};
+
+	let codeLensProviderDisposable = vscode.languages.registerCodeLensProvider(
+		docSelector,
+		new CqhCodeLenProvider()
+	)
+
+	context.subscriptions.push(codeLensProviderDisposable)
+	let disposable = vscode.commands.registerCommand(static_var.cmd_goto, async (document: vscode.TextDocument,
+		range: vscode.Range) => {
+		let runner = new CqhRunner(document, range);
+		await runner.run();
+
+	})
+
 
 	context.subscriptions.push(disposable);
+
+	// 
+	let listDisposable = vscode.commands.registerTextEditorCommand(static_var.cmd_list, async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+		await cqh_goto_list(textEditor, edit);
+	})
+	context.subscriptions.push(listDisposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
